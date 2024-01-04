2ebc4861efb15b8f4755f081bb1f56d54c4cb4ab
NARD <110368170+CocoShesh@users.noreply.github.com>
2024-01-04 15:27:27 +0800

    Update blogData.jsx

--- public/data/blogData.jsx
+++ public/data/blogData.jsx
@@ -1,45 +1,45 @@
 const blog = [
   {
     _id: 1,
-    thumbnail: "/public/assets/blogs/blog-1.jpeg",
+    thumbnail: "/assets/blogs/blog-1.jpeg",
     category: "5D Theatre",
     title: "Dolorum optio tempore voluptas dignissimos",
     author: {
       name: "Maria Doe",
-      image: "/public/assets/blogs/blog-author-1.jpg",
+      image: "/assets/blogs/blog-author-1.jpg",
     },
     date: "Jan 1, 2023",
   },
   {
     _id: 2,
-    thumbnail: "/public/assets/blogs/blog-2.jpeg",
+    thumbnail: "/assets/blogs/blog-2.jpeg",
     category: "Disney",
     title: "Possimus soluta ut id suscipit ea ut in quo quia et soluta",
     author: {
       name: "Allisa Mayer",
-      image: "/public/assets/blogs/blog-author-2.jpg",
+      image: "/assets/blogs/blog-author-2.jpg",
     },
     date: "May 10, 2023",
   },
   {
     _id: 3,
-    thumbnail: "/public/assets/blogs/blog-3.jpeg",
+    thumbnail: "/assets/blogs/blog-3.jpeg",
     category: "Theatre",
     title: "Nisi magni odit consequatur autem nulla dolorem",
     author: {
       name: "Mark Dower",
-      image: "/public/assets/blogs/blog-author-3.jpg",
+      image: "/assets/blogs/blog-author-3.jpg",
     },
     date: "June 23, 2023",
   },
   {
     _id: 4,
-    thumbnail: "/public/assets/blogs/blog-4.jpeg",
+    thumbnail: "/assets/blogs/blog-4.jpeg",
     category: "Theatre Cater",
     title: "Non rem rerum nam cum quo minus olor distincti",
     author: {
       name: "Lisa Neymar",
-      image: "/public/assets/blogs/blog-author-4.jpg",
+      image: "/assets/blogs/blog-author-4.jpg",
     },
     date: "July 23, 2023",
   },
